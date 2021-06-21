2f473569b5903baee7842ebc990b17e28a1dbcee
Mert <mertdemirok80@gmail.com>
2021-06-21 08:54:37 +0200

    add id to customer

--- src/main/java/be/switchfully/model/Customer.java
+++ src/main/java/be/switchfully/model/Customer.java
@@ -1,13 +1,36 @@
 package be.switchfully.model;
 
 import lombok.*;
+import java.util.Objects;
+import java.util.UUID;
 
-@RequiredArgsConstructor
 @Getter
-@Setter
+@RequiredArgsConstructor
 public class Customer {
+    private String id = UUID.randomUUID().toString();
     @NonNull
     private String fullName;
     private int scoreDay;
     private int scoreMonth;
+
+    public void setScoreDay(int scoreDay) {
+        this.scoreDay+=scoreDay;
+    }
+
+    public void setScoreMonth(int scoreMonth) {
+        this.scoreMonth+=scoreMonth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id.equals(customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName);
+    }
 }
