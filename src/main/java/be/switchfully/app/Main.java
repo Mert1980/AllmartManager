9c20ae10671657f19f20a06034201916c42e80e7
Mert <mertdemirok80@gmail.com>
2021-06-21 09:21:45 +0200

    call run method from AllmartUserInterface

---
+++ src/main/java/be/switchfully/app/Main.java
@@ -0,0 +1,9 @@
+package be.switchfully.app;
+
+import be.switchfully.view.AllmartUserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        AllmartUserInterface.run();
+    }
+}
