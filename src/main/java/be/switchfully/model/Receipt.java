186c5c31abe77224a28922239d10de65dc9164f5
Mert <mertdemirok80@gmail.com>
2021-06-21 08:55:19 +0200

    remove nonNull annotations and define a custom constructor

--- src/main/java/be/switchfully/model/Receipt.java
+++ src/main/java/be/switchfully/model/Receipt.java
@@ -4,13 +4,15 @@ import lombok.*;
 import java.time.LocalDate;
 import java.util.HashMap;
 
-@RequiredArgsConstructor
 @Getter
 @Setter
 public class Receipt {
-    @NonNull
     private Customer customer;
     private LocalDate date = LocalDate.now();
-    @NonNull
     private HashMap<Product, Integer> productAmountMap;
+
+    public Receipt(Customer customer, HashMap<Product, Integer> productAmountMap) {
+        this.customer = customer;
+        this.productAmountMap = productAmountMap;
+    }
 }
