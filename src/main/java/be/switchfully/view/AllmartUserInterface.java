7f55eaa04adda7d8f69da31049c39ef1b7b6a23b
Mert <mertdemirok80@gmail.com>
2021-06-22 11:34:40 +0200

    refactor name as firstName and lastName

--- src/main/java/be/switchfully/view/AllmartUserInterface.java
+++ src/main/java/be/switchfully/view/AllmartUserInterface.java
@@ -15,7 +15,6 @@ import java.util.Timer;
 public class AllmartUserInterface {
     private static CashierService cashierService = AllmartServiceFactory.getCashierService();
     private static CustomerService customerService = AllmartServiceFactory.getCustomerService();
-    private static ReportingService reportingServiceDaily = AllmartServiceFactory.getReportingServiceDaily();
     private static ReportingService reportingServiceMonthly = AllmartServiceFactory.getReportingServiceMonthly();
     private static boolean isCashRegisterOpen = true;
     private static boolean isMoreProduct = true;
@@ -37,34 +36,38 @@ public class AllmartUserInterface {
         DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         Date date = null;
         try {
-            date = dateFormatter.parse("2021-06-21 19:30");
+            date = dateFormatter.parse("2021-06-24 19:30");
         } catch (ParseException e) {
             e.printStackTrace();
         }
         Timer timer = new Timer(false);
-        timer.schedule(scheduler, date, 40_000);
+        timer.schedule(scheduler, date, 86_400_000);
     }
 
     private static void openCashRegister(){
 
         while (isCashRegisterOpen){
 
-            System.out.println("Enter Customer FullName:  'X' closes the cash register ");
-            String input = scanner.nextLine();
+            System.out.println("Enter customer first name OR enter 'X' to close the cash register ");
+            String firstName = scanner.nextLine();
 
-            if(!isSupermarketOpen(input)) break;
+            if(!isSupermarketOpen(firstName)) break;
 
-            HashMap<Product, Integer> productAmountMap = new HashMap<>();
+            System.out.println("Enter customer last name: ");
+            String lastName = scanner.nextLine();
+
             Customer customer;
 
-            if(customerService.findByCustomerName(input) != null){
-                customer = customerService.findByCustomerName(input);
+            if(customerService.findByCustomerName(firstName + lastName) != null){
+                customer = customerService.findByCustomerName(firstName + lastName);
             } else {
-                customer = new Customer(input);
+                customer = new Customer(firstName, lastName);
             }
 
+            HashMap<Product, Integer> productAmountMap = new HashMap<>();
+
             while(isMoreProduct){
-                System.out.println("Enter product name: 'X' if no more product" );
+                System.out.println("Enter product name OR enter 'N' if no more product" );
                 String productName = scanner.nextLine();
 
                 if(!checkMoreProduct(productName)) break;
@@ -82,17 +85,15 @@ public class AllmartUserInterface {
     }
 
     private static void askForMonthlyReport() {
-        System.out.println("Do you want to print customer of the month report? Y/N");
-        switch (scanner.next()) {
-            case "Y":
-                reportingServiceMonthly.generateReport();
-                break;
-            default:break;
+        System.out.println("Enter 'Y' to print customer of the month report?");
+        String input = scanner.nextLine();
+        if(input.equalsIgnoreCase("Y")) {
+            reportingServiceMonthly.generateReport();
         }
     }
 
     private static boolean checkMoreProduct(String input) {
-        if (input.equalsIgnoreCase("X")) {
+        if (input.equalsIgnoreCase("N")) {
             isMoreProduct = false;
         }
         return isMoreProduct;
